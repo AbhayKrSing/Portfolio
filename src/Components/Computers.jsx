59712f062e7c19f6d2e82aae7b8df9b376ea859d
Abhay <kabhay849@gmail.com>
2023-10-11 20:16:13 +0530

    Done

--- src/Components/Computers.jsx
+++ src/Components/Computers.jsx
@@ -1,16 +1,29 @@
-import React from "react";
-import { Canvas } from "@react-three/fiber";
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 import styled from "styled-components";
 
 const Computers = () => {
-  const Computer = useGLTF("/desktop_pc/scene.gltf");
+  const meshRef = useRef();
   const Wrapper = styled.div`
     canvas {
-      max-height: 400px;
       width: 80%;
     }
   `;
+  const Mesh = () => {
+    // useFrame(()=>{
+    //  meshRef.current.position()
+    // })
+    React.useEffect(() => {
+      meshRef.current.position.set(-5, -2, 3);
+    }, []);
+    const Computer = useGLTF("/desktop_pc/scene.gltf");
+    return (
+      <mesh scale={1} ref={meshRef}>
+        <primitive object={Computer.scene} />
+      </mesh>
+    );
+  };
   return (
     <Wrapper>
       <Canvas
@@ -27,9 +40,7 @@ const Computers = () => {
         />
         <hemisphereLight intensity={0.15} />
         <pointLight intensity={1} />
-        <mesh scale={1}>
-          <primitive object={Computer.scene} />
-        </mesh>
+        <Mesh></Mesh>
       </Canvas>
     </Wrapper>
   );
