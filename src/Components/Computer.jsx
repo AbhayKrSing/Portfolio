4162bc95caee7ceec65bc5a799ba13155a2e3ccc
Abhay <kabhay849@gmail.com>
2023-10-13 00:16:03 +0530

    again computer object loaded successfully

--- src/Components/Computer.jsx
+++ src/Components/Computer.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls, useGLTF } from "@react-three/drei";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import { Canvas, useLoader } from "@react-three/fiber";
 import React, { useRef, Suspense } from "react";
 import { TextureLoader } from "three";
@@ -8,10 +8,27 @@ import CanvasLoader from "./Loader";
 const Mesh = (props) => {
   const colormap = useLoader(TextureLoader, texture);
   const meshRef = useRef();
+  const computer = useGLTF("/desktop_pc/scene.gltf");
   return (
-    <mesh rotation={[90, 0, 20]} scale={1}>
-      <boxGeometry args={[3, 3, 3]} />
-      <meshStandardMaterial map={colormap} />
+    <mesh scale={1}>
+      {/* <boxGeometry args={[3, 3, 3]} />
+      <meshStandardMaterial map={colormap} /> */}
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <pointLight intensity={1} />
+      <primitive
+        object={computer.scene}
+        scale={1}
+        position={[0, -2.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
     </mesh>
   );
 };
@@ -23,23 +40,32 @@ const Computer = () => {
       grid-gap: 1rem;
       place-items: start;
       position: relative;
-      right: 3rem;
       bottom: 3rem;
     }
   `;
-  const computer = useGLTF("/desktop_pc/scene.gltf");
+
   return (
     <Wrapper>
       <Canvas
         className="canvas"
-        style={{ height: "400px", width: "min-content" }}
+        style={{ height: "60vh", width: "100%" }}
+        frameloop="demand"
+        shadows
+        dpr={[1, 2]}
+        camera={{ position: [20, 3, 5], fov: 25 }}
+        gl={{ preserveDrawingBuffer: true }}
       >
-        <OrbitControls enableZoom={false} />
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
         <ambientLight intensity={1} />
         <directionalLight position={[10, 10, 5]} intensity={2} />
         <Suspense fallback={<CanvasLoader />}>
           <Mesh />
         </Suspense>
+        <Preload all />
       </Canvas>
     </Wrapper>
   );
