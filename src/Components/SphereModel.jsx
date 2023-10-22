156156b4d564d6ab55bba571b8449c39fbeb5585
Abhay <kabhay849@gmail.com>
2023-10-22 19:41:47 +0530

    done as of now

--- src/Components/SphereModel.jsx
+++ src/Components/SphereModel.jsx
@@ -1,14 +1,17 @@
 // import { RepeatWrapping } from "three";
-import React, { Suspense } from "react";
+import React, { Suspense, useRef } from "react";
 import { Float, OrbitControls } from "@react-three/drei";
 import { Canvas, useLoader } from "@react-three/fiber";
 import { TextureLoader } from "three";
 import { TechStack } from "../assets";
+import { textvariants } from "../variants/index";
+import { motion, useInView } from "framer-motion";
 
 function AnimatedSphere({ image }) {
   const colormap = useLoader(TextureLoader, image);
   // colormap.wrapS = colormap.wrapT = RepeatWrapping;
   // colormap.repeat.set(1, 1); // Adjust these values to change the texture size
+
   return (
     <Float>
       <mesh rotation={[90, 0, 20]} scale={1.2}>
@@ -33,16 +36,24 @@ function SphereModel({ image }) {
 }
 
 export default function SphereShape() {
+  const Introref = useRef();
+  const IntroView = useInView(Introref);
   return (
     <>
-      <h2 className="text-xl font-semibold mt-10">
-        <span className="text-secondary font-light tracking-widest">
-          TechStacks
-        </span>
-      </h2>
-      <h1 className="text-6xl font-black my-6">
-        <span className="text-secondary">My TechStacks</span>
-      </h1>
+      <motion.div
+        ref={Introref}
+        variants={textvariants}
+        animate={IntroView ? "visible" : "hidden"}
+      >
+        <h2 className="text-xl font-semibold">
+          <span className="text-secondary font-light tracking-widest">
+            TechStacks
+          </span>
+        </h2>
+        <h1 className="text-6xl font-black my-6">
+          <span className="text-secondary">My TechStacks.</span>
+        </h1>
+      </motion.div>
       <div className="flex space-x-10 flex-wrap">
         <div></div>
         {TechStack.map((element, index) => {
